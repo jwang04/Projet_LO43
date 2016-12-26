b0251b29c62d9f25b11ea4d3c883bc42f34daa06
LIUJINGYUYVES <18717898253@163.com>
2016-12-26 11:53:21 +0100

    Update Lignes.java

--- Mini-Metro/src/Game/Lignes.java
+++ Mini-Metro/src/Game/Lignes.java
@@ -1,4 +1,48 @@
 package Game;
 
 public class Lignes {
+  private Stations Start = new Stations();
+  private Stations Terminal = new Stations();
+  private Color col = new Color();
+  
+  public Color GetColor(){
+    return col;
+  }
+  
+  public Stations GetStart(){
+    return Start;
+  }
+  
+  public Stations GetTerminal(){
+    return Terminal;
+  }
+  
+  public void SetColor(Color c){
+    ////
+  }
+  
+  public void SetStart(Stations s){
+    ////
+  }
+  
+  public void SetTerminal(Stations t){
+    ////
+  }
+  
+  public void NextRoutes(Trains train){
+   //// 
+  }
+  
+  public void Transfer(Trains train, Stations station){
+   //// 
+  }
+  
+  public boolean UTurn(){
+    ////
+  }
+  
+  public boolean IsCircular(){
+    ////
+  }
+  
 }
