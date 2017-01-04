85c4ed18eae298d36d94719ae75de9289c7eb8ec
HUANG Michel <HUANG Michel@192.168.1.13>
2017-01-04 22:02:23 +0100

    ADD FENETRECTRL AND FENETREVUE

---
+++ Mini-Metro/src/Model/GameLauncher.java
@@ -0,0 +1,18 @@
+package Model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GameLauncher implements Runnable {
+	private Map map;
+	private Times clock;
+	private List<Inventaire> inventaire = new LinkedList<Inventaire>();
+	private List<Lignes> ligne = new LinkedList<Lignes>();
+	private List<Passagers> passager = new LinkedList<Passagers>();
+	private List<Trains> train = new LinkedList<Trains>();
+	private List<Wagons> wagon = new LinkedList<Wagons>();
+	
+	public void run(){
+		
+	}
+}
---
+++ Mini-Metro/src/View/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author HUANG Michel
+ *
+ */
+package View;
