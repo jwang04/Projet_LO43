5713cfa4b711bcb0b3dc5896759d40e31d7edff5
HUANG Michel <HUANG Michel@192.168.1.13>
2017-01-05 17:17:46 +0100

    Modif files and add files buttonview, cercleview...

--- Mini-Metro/src/Controller/FenetreCtrl.java
+++ Mini-Metro/src/Controller/FenetreCtrl.java
@@ -12,10 +12,10 @@ import javafx.scene.control.Button;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
-
 public class FenetreCtrl extends Application{
 	
 	FenetreVue fenetre = new FenetreVue();
+	ButtonView button = new ButtonView();
 	
 	 public static void main(String[] args) {
 	        launch(args);
@@ -23,6 +23,7 @@ public class FenetreCtrl extends Application{
 
 	    @Override
 	    public void start(Stage stage) {
+	    	
 	        Group root = new Group();
 	        //exemple d'application
 	        //Circle circle = new Circle(30,30,30);
@@ -31,35 +32,37 @@ public class FenetreCtrl extends Application{
 	        //circle.setTranslateY(400);
 	        //root.getChildren().add(circle);
 	        
-	        fenetre.GetBtn_Close().setOnAction(new EventHandler<ActionEvent>(){
-	        	@Override
-	        	public void handle(ActionEvent event){
-	        		stage.close();
-	        	}
-	        });
+	        Btn_Event(stage);
 	        
-	        fenetre.GetBtn_Start().setOnAction(new EventHandler<ActionEvent>(){
-
-				@Override
-				public void handle(ActionEvent event) {
-					// TODO Auto-generated method stub
-					
-				}
-	        	
-	        });
+	        button.SetBtn_Start();
+	        button.SetBtn_Close();
 	        
-	        fenetre.SetBtn_Start();
-	        fenetre.SetBtn_Close();
-	        
-	        
-	        root.getChildren().add(fenetre.GetBtn_Close());
-	        root.getChildren().add(fenetre.GetBtn_Start());
-	        
+	        root.getChildren().addAll(button.GetBtn_Close(), button.GetBtn_Start(), fenetre.GetIV());
 	        Scene scene = new Scene(root, fenetre.GetWitdh(), fenetre.GetHeight(), Color.TRANSPARENT);
 	        stage.setTitle(fenetre.GetTitle());
 	        
 	        stage.setScene(scene);
 	        stage.show();
+	    }
+	    
+	    public void Btn_Event(Stage stage){
+	    	
+	    	 button.GetBtn_Close().setOnAction(new EventHandler<ActionEvent>(){
+		        	@Override
+		        	public void handle(ActionEvent event){
+		        		stage.close();
+		        	}
+		        });
+		        
+		        button.GetBtn_Start().setOnAction(new EventHandler<ActionEvent>(){
+
+					@Override
+					public void handle(ActionEvent event) {
+						// TODO Auto-generated method stub
+						
+					}
+		        	
+		        });
 	    }
 
 }
--- Mini-Metro/src/View/FenetreVue.java
+++ Mini-Metro/src/View/FenetreVue.java
@@ -1,14 +1,28 @@
 package View;
 
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class FenetreVue {
 	private double Witdh = 1200;
 	private double Height = 800;
 	private String Title = "Mini-Metro";
-	Button btn_start = new Button();
-	Button btn_close = new Button();
+	private String URL = "file:/Background.png";
 	
+	Image ImageBack = new Image(URL, true);
+	ImageView iv = new ImageView();
+	
+	public void SetImage(){
+		iv.setImage(ImageBack);
+	}
+	
+	public ImageView GetIV(){
+		return iv;
+	}
+	
+	public Image GetImage(){
+		return ImageBack;
+	}
 	
 	public double GetWitdh() {
 		return Witdh;
@@ -22,23 +36,4 @@ public class FenetreVue {
 		return Title;
 	}
 	
-	public Button GetBtn_Start(){
-		return btn_start;
-	}
-	
-	public void SetBtn_Start(){
-		btn_start.setText("Commencer");
-		btn_start.setTranslateX(600);
-        btn_start.setTranslateY(400);
-	}
-	
-	public Button GetBtn_Close(){
-		return btn_close;
-	}
-	
-	public void SetBtn_Close(){
-		btn_close.setText("Quitter");
-		btn_close.setTranslateX(600);
-        btn_close.setTranslateY(500);
-	}
 }
