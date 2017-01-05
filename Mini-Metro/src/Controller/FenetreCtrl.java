8e9fcdcde0ab9b2610f57f1ca98a38ac4c250d29
HUANG Michel <HUANG Michel@172.18.19.135>
2017-01-05 11:14:49 +0100

    Update

--- Mini-Metro/src/Controller/FenetreCtrl.java
+++ Mini-Metro/src/Controller/FenetreCtrl.java
@@ -7,6 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.control.Button;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
 
 public class FenetreCtrl extends Application{
 	
@@ -22,7 +29,30 @@ public class FenetreCtrl extends Application{
 	        //exemple d'application
 	        //Circle circle = new Circle(30,30,30);
 	        //circle.setFill(Color.BLACK);
+	        //circle.setTranslateX(600);
+	        //circle.setTranslateY(400);
 	        //root.getChildren().add(circle);
+	        
+	        Button btn_start = new Button();
+	        btn_start.setText("Commencer");
+	        Button btn_close = new Button();
+	        btn_close.setText("Quitter");
+	        btn_close.setOnAction(new EventHandler<ActionEvent>(){
+	        	@Override
+	        	public void handle(ActionEvent event){
+	        		stage.close();
+	        	}
+	        });
+	        btn_start.setTranslateX(600);
+	        btn_start.setTranslateY(400);
+	        btn_start.setMaxWidth(500);
+	        btn_close.setTranslateX(600);
+	        btn_close.setTranslateY(500);
+	        root.getChildren().add(btn_close);
+	        root.getChildren().add(btn_start);
+	        //Image image = new Image("/header.jpg",true);
+	       // Image image = new Image("header.jpg");
+	        //BackgroundImage imageB = new BackgroundImage(image, null, null, null,null);
 	        Scene scene = new Scene(root, width, height, Color.TRANSPARENT);
 	        stage.setTitle("Mini-Metro");
 	        
